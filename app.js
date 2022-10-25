d90d7e43ec5b78e6671b3f590ee7d922a3c6c79d
AbduazimAbduvaliyev <aabduvaliyev180@example.com>
2022-10-25 14:16:36 +0500

    boshladik

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+const port = 3035
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const rIndex = require("./routers/index")
+
+mongoose.connect("mongodb://localhost:27017/Moves")
+const db = mongoose.connection
+db.on("open", ()=>{
+    console.log("mongodb running");
+})
+db.on("error", ()=>{
+    console.log("mongodb is wrong");
+
+})
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded)
+
+app.get("/", (req, res )=>{
+    res.send("Hello world")
+})
+
+app.use(rIndex )
+
+
+app.listen(port , ()=>{
+    console.log("server running");
+})
---
+++ routers/index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const router = express.Router()
+const movieDb = require("./model/movie")
+
+router.get("/api/movies" ,(req , res )=>{
+     movieDb.find({}, (error, data )=>{
+        res.send
+     })
+})
+
+router.post("/api/movies" , (req, res )=>{
+   const {title, category, country,director, imdb_score, year}=req.body
+   const db = new movieDb({
+      title: title,
+      category: category,
+      country: country,
+      director:director,
+      imdb_score:imdb_score,
+      year : year
+   })
+   db.save()
+})
+
+router.post("/api/movies/:id" , (req, res )=>{
+   movieDb.findById{req.params.id, (err, data)=>{
+      res.send(data)
+   }}
+})
+router.put("/api/movies/:id" , (req , res)=>{
+const {title, category, country, director, imdb_score,   year}=req.body
+
+const db = {
+   title : title,
+   category: category,
+   country: country,
+   director: director,
+   imdb_score:imdb_score,
+   year: year
+}
+
+   movieDb.findByIdAndUpdate(req.params.id, db , (err, data)=>{
+      res.send(data)
+   })
+
+})
+
+
+module.exports = router
